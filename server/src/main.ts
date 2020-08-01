75a359f67c193097d5e125328f6a6dad3db9e23e
Michał Mirończuk <michalmironczuk1@gmail.com>
2020-08-01 10:44:54 +0200

    Added ValidationPipe

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   app.enableCors(); 
   await app.listen(3001);
 }
